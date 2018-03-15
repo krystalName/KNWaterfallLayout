1bb98651a3e8316d649c620ebad210cb0c7cbec0
Leessang <992275294@qq.com>
2018-03-15 14:49:42 +0800

    Initial Commit

---
+++ KNWaterfallLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNWaterfallLayout
+//
+//  Created by 刘凡 on 2018/3/15.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
